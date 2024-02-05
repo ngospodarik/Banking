f7021a29d22ceb53206992263097fe185fd78cc4
nikolaygospodarik <n@gospodarik.ru>
2024-02-05 18:23:20 +0300

    Added run method

--- src/Bank.java
+++ src/Bank.java
@@ -8,7 +8,20 @@ public class Bank {
 
     public static void main(String[] args) {
 
+        // creates new instance of Bank class
+        Bank bank = new Bank();
+
+        // calls the run method in the bank class
+        bank.run();
+
 
     }
 
+    /**
+     * Runs the program by initializing and managing bank account and customers.
+     */
+    public void run() {
+
+
+    }
 }
