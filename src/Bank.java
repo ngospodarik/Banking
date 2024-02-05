016b023e66b7d59e3136898e9ffbf14c67eb6961
nikolaygospodarik <n@gospodarik.ru>
2024-02-05 18:17:01 +0300

    Initial commit

---
+++ src/Bank.java
@@ -0,0 +1,14 @@
+package banking;
+
+/**
+ * Represents a bank for managing customers and their bank accounts
+ */
+
+public class Bank {
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
