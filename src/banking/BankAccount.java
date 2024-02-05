c6cb108610397c8509d28ff08e6df8f0f28389c9
nikolaygospodarik <n@gospodarik.ru>
2024-02-05 18:27:13 +0300

    Added class BankAccount

---
+++ src/banking/BankAccount.java
@@ -0,0 +1,26 @@
+package banking;
+
+/**
+ * Represents a checking/savings bank account for a customer
+ */
+
+public class BankAccount {
+
+    // instance vars
+
+    /**
+     * Type of account (checking/savings)
+     */
+    String accountType;
+
+    /**
+     * Balance for bank account
+     */
+    double balance;
+
+    /**
+     * Customer for this account
+     */
+    Customer customer;
+
+}
